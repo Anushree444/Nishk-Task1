a3a5b1f4a15b151fa3dea9fb7a495551be8a9c36
Anushree444 <ec24b1119@iiitdm.ac.in>
2025-04-15 16:03:23 +0530

    Update mars_emergency_flap_system1.ino

--- mars_emergency_flap_system1.ino
+++ mars_emergency_flap_system1.ino
@@ -1,7 +1,7 @@
 //project 3: mars emergency flap with potentiometer and push button
 #include <Servo.h>
 
-const int tempInput = A0;       // Potentiometer simulating temperature
+const int tempInput = A0;       // temp sensor simulating temperature
 const int ledPin = 7;           // Warning LED
 const int servoPin = 9;         // Servo motor
 const int buttonPin = 4;        // Push button for manual emergency flap
